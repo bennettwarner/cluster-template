ff5e46e26ed6544c63de8a361c21c04f9c0325f2
codyfremont <41700866+codyfremont@users.noreply.github.com>
2018-10-04 18:58:22 -0400

    Update and rename setupNFS_compute.sh to mountHead.sh
    changed ish

--- mountHead.sh
+++ mountHead.sh
@@ -1,6 +1,4 @@
 mount -t nfs 192.168.1.1:/software /mnt/software
-mount -t nfs 192.168.1.3:/scratch /mnt/scratch
 
 vi /etc/fstab
 192.168.1.1:/software /mnt  nfs defaults 0 0
-192.168.1.3:/scratch /mnt  nfs defaults 0 0
