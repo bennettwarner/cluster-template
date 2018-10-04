76879ae0170d37c02029d3a107a1e6d13abe8d6d
codyfremont <41700866+codyfremont@users.noreply.github.com>
2018-10-04 18:35:16 -0400

    Create setupNFS-Storage.sh
    Setup NFS for storage node, must be run before client's mount shared dir

---
+++ setupNFS-Storage.sh
@@ -0,0 +1,27 @@
+sudo yum install nfs-utils nfs-utils-lib
+sudo yum install portmap
+
+/etc/init.d/portmap start
+/etc/init.d/nfs start
+chkconfig --level 35 portmap on
+chkconfig --level 35 nfs on
+
+mkdir /scratch
+vi /etc/exports
+/software 10.10.10.3(rw,sync,no_root_squash)
+/software 10.10.10.4(rw,sync,no_root_squash)
+/software 10.10.10.5(rw,sync,no_root_squash)
+/software 10.10.10.6(rw,sync,no_root_squash)
+/software 10.10.10.7(rw,sync,no_root_squash)
+/software 10.10.10.8(rw,sync,no_root_squash)
+/software 10.10.10.9(rw,sync,no_root_squash)
+/software 10.10.10.10(rw,sync,no_root_squash)
+/software 10.10.10.11(rw,sync,no_root_squash)
+/software 10.10.10.12(rw,sync,no_root_squash)
+/software 10.10.10.13(rw,sync,no_root_squash)
+/software 10.10.10.14(rw,sync,no_root_squash)
+/software 10.10.10.15(rw,sync,no_root_squash)
+
+
+#info found:
+#https://www.tecmint.com/how-to-setup-nfs-server-in-linux/
