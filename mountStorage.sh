3e0c7ab813451d8d6845b0f0228aae8eca38fdcf
codyfremont <41700866+codyfremont@users.noreply.github.com>
2018-10-17 13:34:13 -0400

    Update mountStorage.sh
    added install line

--- mountStorage.sh
+++ mountStorage.sh
@@ -1,4 +1,7 @@
-mount -t nfs 192.168.1.3:/scratch /mnt/scratch
+set -x
+
+sudo yum -y install nfs-utils-lib
+sudo mount -t nfs 192.168.1.3:/scratch /mnt/scratch
 
 vi /etc/fstab
 192.168.1.3:/scratch /mnt  nfs defaults 0 0
