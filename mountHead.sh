afa7f5e369cb1f2baab343353b8592d09bbaf3ca
mrBlint <blintsan@gmail.com>
2018-10-18 00:35:33 -0400

    hm

--- mountHead.sh
+++ mountHead.sh
@@ -7,10 +7,8 @@ sudo mkdir -p /users/al844976/scratch
 
 sudo mount -t nfs 192.168.1.1:/software /users/al844976/software
 sudo mount -t nfs 192.168.1.3:/software /users/al844976/scratch
-echo '
-192.168.1.1:/software /users/al844976/software  nfs defaults 0 0' >> /etc/fstab
-echo '
-192.168.1.3:/scratch /users/al844976/scratch  nfs defaults 0 0' >> /etc/fstab
+echo "192.168.1.1:/software /users/al844976/software  nfs defaults 0 0" >> /etc/fstab
+echo "192.168.1.3:/scratch /users/al844976/scratch  nfs defaults 0 0" >> /etc/fstab
 
 echo "export PATH='$PATH:/users/al844976/software/openmpi/3.1.2/bin'" >> /users/al844976/.bashrc
 echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/users/Js637496/software/openmpi/3.1.2/lib/'" >> /users/al844976/.bashrc
