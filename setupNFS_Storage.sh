b1934a0d6c0a2cbf9e3717b7b10c03718a7d5095
mrBlint <blintsan@gmail.com>
2018-10-18 18:42:20 -0400

    del

--- setupNFS_Storage.sh
+++ setupNFS_Storage.sh
@@ -7,9 +7,6 @@ sudo yum -y install portmap
 sudo chkconfig --level 35 portmap on
 sudo chkconfig --level 35 nfs on
 
-sudo mkdir /scratch
-sudo chmod 777 /scratch
-
 sudo systemctl enable nfs-server.service
 sudo systemctl start nfs-server.service
 
