1016de3aa03dcc2e6d1037088e8f4f0cab9bd730
mrBlint <blintsan@gmail.com>
2018-10-18 18:08:08 -0400

    Create client.sh

---
+++ client.sh
@@ -0,0 +1,15 @@
+set -x
+sudo yum -y install nfs-utils nfs-utils-lib
+
+
+sudo mkdir -p /software
+sudo mkdir -p /scratch
+sudo chmod 777 /software
+sudo chmod 777 /scratch
+sudo mount -t nfs 192.168.1.1:/software /software
+sudo mount -t nfs 192.168.1.3:/scratch /scratch
+echo "192.168.1.1:/software /software  nfs defaults 0 0" >> /etc/fstab
+echo "192.168.1.3:/scratch /scratch  nfs defaults 0 0" >> /etc/fstab
+
+echo "export PATH='$PATH:/software/openmpi/3.1.2/bin'" >> /users/al844976/.bashrc
+echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/software/openmpi/3.1.2/lib/'" >> /users/al844976/.bashrc
