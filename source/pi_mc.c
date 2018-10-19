de9cc54012e97a885c840f2d6a9a89c556306055
codyfremont <41700866+codyfremont@users.noreply.github.com>
2018-10-18 20:29:50 -0400

    Update pi_mc.c

--- source/pi_mc.c
+++ source/pi_mc.c
@@ -1,70 +1,62 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
 #include <math.h>
 
-int main(int argc, char* argv[]) 
-{
+int main(int argc, char* argv[]) {
 
-  int CirclePoints = 0;
+  int nPointsInCircle  = 0;
   int i = 0;
-  int TotalPoints = 0;
-  int PointsInRegion = 0;
-  int PointsRecv = 0;
+  int nPointsTotal     = 0;
+  int nPointsPerRegion = 0;
+  int pointsReceived   = 0;
   double piEstimate;
   double x_start, y_start;
   double x_rand, y_rand, rand_radius;
-  double StartTime, StopTime, TotalTime;
-  int MPI_rank, size, SquareWidth;
-  double SquareDouble;
+  double start, stop, tpar;
+  int rank, size, squareWidth;
+  double sqWDub;
   MPI_Status status;
 
-  TotalPoints = atoi(argv[1]);
+  nPointsTotal = atoi(argv[1]);
 
   MPI_Init(&argc, &argv);
-  MPI_Comm_rank(MPI_COMM_WORLD, &MPI_rank);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-  StartTime = MPI_Wtime();
-  PointsInRegion = TotalPoints / size;
-  srand( (unsigned)time(NULL) + MPI_rank );
-  SquareWidth = (int) sqrt(size);
-  SquareDouble = (int) sqrt(size);
+  // Seed RNG and make calculations for constants
+  start = MPI_Wtime();
+  nPointsPerRegion = nPointsTotal / size;
+  srand( (unsigned)time(NULL) + rank ); // seed differently per node
+  squareWidth = (int) sqrt(size);
+  sqWDub = (int) sqrt(size);
+  // Place and record points in the circle
+  x_start = (double)(rank % squareWidth) / squareWidth;
+  y_start = (double)((rank / squareWidth)) / squareWidth;
 
-  // Specify the points in the circle
-  x_start = (double)(MPI_rank % SquareWidth) / SquareWidth;
-  y_start = (double)((MPI_rank / SquareWidth)) / SquareWidth;
+  //printf("Rank %d out of %d has starting x %f and starting y %f on a square of size %d \n",
+ //       rank, size, x_start, y_start, squareWidth);
 
-  printf("Rank %d of %d has started x %f and starting y %f on a square of size %d \n",
-         MPI_rank, size, x_start, y_start, SquareWidth);
-
-  for (i = 0; i < PointsInRegion; i++) 
-  {
-    x_rand = (double)rand() / ((double)RAND_MAX * SquareWidth) + x_start;
-    y_rand = (double)rand() / ((double)RAND_MAX * SquareWidth) + y_start;
-    rand_radius = (x_rand - (1/SquareDouble)) * (x_rand - (1/SquareDouble)) + (y_rand - (1/SquareDouble)) * (y_rand - (1/SquareDouble));
-    if (rand_radius <= ((1/SquareDouble) * (1/SquareDouble))) 
-	{
-      CirclePoints += 1;
+  for (i = 0; i < nPointsPerRegion; i++) {
+    x_rand = (double)rand() / ((double)RAND_MAX * squareWidth) + x_start;
+    y_rand = (double)rand() / ((double)RAND_MAX * squareWidth) + y_start;
+    rand_radius = (x_rand - (1/sqWDub)) * (x_rand - (1/sqWDub)) + (y_rand - (1/sqWDub)) * (y_rand - (1/sqWDub));
+    if (rand_radius <= ((1/sqWDub) * (1/sqWDub))) {
+      nPointsInCircle += 1;
     }
   }
-
-  StopTime = MPI_Wtime();
-  TotalTime = StopTime - StartTime;
+  stop = MPI_Wtime();
+  tpar = stop - start;
   
-  MPI_Reduce(&CirclePoints, &PointsRecv, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-  if (MPI_rank == 0) 
-  {
-    piEstimate = (double)(PointsRecv * size) / TotalPoints;
-    if (size < 4) 
-	{
+  MPI_Reduce(&nPointsInCircle, &pointsReceived, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+  if (rank == 0) {
+    piEstimate = (double)(pointsReceived * size) / nPointsTotal;
+    if (size < 4) {
         piEstimate = piEstimate * 2;
     }
-    printf("the estimation of PI is: %f\n Process %d took: %lf \n", piEstimate, MPI_rank, TotalTime);
+    printf("the estimation of PI is: %f\n Process %d took: %lf \n", piEstimate, rank, tpar);
   }
 
-	
   MPI_Finalize();
   return 0;
 }
