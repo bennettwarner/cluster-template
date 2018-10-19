7b9ac659b07f7b651441f92b576bd8c93440ba65
mrBlint <blintsan@gmail.com>
2018-10-18 20:43:06 -0400

    Update mountHead.sh

--- mountHead.sh
+++ mountHead.sh
@@ -1,17 +1,17 @@
 set -x
 sudo yum -y install nfs-utils nfs-utils-lib
 
+sudo mkdir -p /users/al844976/software
+sudo chmod 777 /users/al844976/software
 
-sudo mkdir -p /software
-sudo mkdir -p /scratch
+sudo mkdir -p /users/al844976/scratch
+sudo chmod 777 /users/al844976/scratch
 
-sudo chmod 777 /software
-sudo chmod 777 /scratch
+sudo mount -t nfs 192.168.1.1:/software /users/al844976/software
+sudo mount -t nfs 192.168.1.3:/scratch /users/al844976/scratch
 
-sudo mount -t nfs 192.168.1.1:/software /software
-sudo mount -t nfs 192.168.1.3:/scratch /scratch
-echo "192.168.1.1:/software /software  nfs defaults 0 0" >> /etc/fstab
-echo "192.168.1.3:/scratch /scratch  nfs defaults 0 0" >> /etc/fstab
+echo '192.168.1.1:/software /users/al844976/software  nfs defaults 0 0' >> /etc/fstab
+echo '192.168.1.3:/scratch /users/al844976/scratch  nfs defaults 0 0' >> /etc/fstab
 
-echo "export PATH='$PATH:/software/openmpi/3.1.2/bin'" >> /users/al844976/.bashrc
-echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/software/openmpi/3.1.2/lib/'" >> /users/al844976/.bashrc
+echo "export PATH='$PATH:/users/al844976/software/openmpi/3.1.2/bin'" >> /users/al844976/.bashrc
+echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/users/al844976/software/openmpi/3.1.2/lib/'" >> /users/al844976/.bashrc
