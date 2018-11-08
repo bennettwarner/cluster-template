09517f0faba4c2a5b5e9e3927042c52360f38cbf
mrBlint <blintsan@gmail.com>
2018-11-08 17:20:49 -0500

    plssssss

--- mountHead.sh
+++ mountHead.sh
@@ -1,8 +1,8 @@
 set -x
 sudo yum -y install nfs-utils nfs-utils-lib
 
-sudo mkdir -p /users/al844976//software
-sudo chmod 777 /users/al844976//software
+sudo mkdir -p /users/al844976/software
+sudo chmod 777 /users/al844976/software
 
 sudo mkdir -p /scratch
 sudo chmod 777 /scratch
@@ -10,7 +10,7 @@ sudo chmod 777 /scratch
 sudo systemctl enable nfs-server
 sudo service nfs start
 
-sudo mount -t nfs 192.168.1.1:/software /users/al844976//software
+sudo mount -t nfs 192.168.1.1:/software /users/al844976/software
 sudo mount -t nfs 192.168.1.3:/scratch /scratch
 
 echo '192.168.1.1:/software /users/al844976/software  nfs defaults 0 0' >> /etc/fstab
