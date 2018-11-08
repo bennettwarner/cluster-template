7dc730ea12997c4275c927caaa72ace333b41b2b
mrBlint <blintsan@gmail.com>
2018-11-08 17:20:23 -0500

    let's hope mpi works.

--- mountHead.sh
+++ mountHead.sh
@@ -1,8 +1,8 @@
 set -x
 sudo yum -y install nfs-utils nfs-utils-lib
 
-sudo mkdir -p /software
-sudo chmod 777 /software
+sudo mkdir -p /users/al844976//software
+sudo chmod 777 /users/al844976//software
 
 sudo mkdir -p /scratch
 sudo chmod 777 /scratch
@@ -10,11 +10,11 @@ sudo chmod 777 /scratch
 sudo systemctl enable nfs-server
 sudo service nfs start
 
-sudo mount -t nfs 192.168.1.1:/software /software
+sudo mount -t nfs 192.168.1.1:/software /users/al844976//software
 sudo mount -t nfs 192.168.1.3:/scratch /scratch
 
-echo '192.168.1.1:/software /software  nfs defaults 0 0' >> /etc/fstab
+echo '192.168.1.1:/software /users/al844976/software  nfs defaults 0 0' >> /etc/fstab
 echo '192.168.1.3:/scratch /scratch  nfs defaults 0 0' >> /etc/fstab
 
 echo "export PATH='$PATH:/software/openmpi/3.1.2/bin'" >> /users/al844976/.bashrc
-echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/software/openmpi/3.1.2/lib/'" >> /users/al844976/.bashrc
+echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/users/al844976/software/openmpi/3.1.2/lib/'" >> /users/al844976/.bashrc
