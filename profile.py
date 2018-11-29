f27af1d19a2774922a4b9dea7d231a296f10473b
mrBlint <blintsan@gmail.com>
2018-11-29 17:47:01 -0500

    4 nodes

--- profile.py
+++ profile.py
@@ -36,7 +36,7 @@ prefixForIP = "192.168.1."
 
 link = request.LAN("lan")
 
-for i in range(0,15):
+for i in range(0,4):
   if i == 0:
     node = request.XenVM("head")
     node.routable_control_ip = "true"
