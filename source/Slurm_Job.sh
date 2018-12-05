06a4ff61acbcb4382d2d5b5b4b78eda0809264f7
codyfremont <41700866+codyfremont@users.noreply.github.com>
2018-12-05 10:03:11 -0500

    Update Slurm_Job.sh
    refactor for Pi_mc.c scaling

--- source/Slurm_Job.sh
+++ source/Slurm_Job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -t 1:00:00
-#SBATCH -N 3
+#SBATCH -N 12
 #SBATCH --ntasks-per-node 4
 
 #echo command to stdout
@@ -11,11 +11,28 @@ echo $SLURM_SUBMIT_DIR
 
 module load mpi/gcc_openmpi
 
-cd $scratch
-mpicc pi_mc.c
+#may need to be edited for proper dir
+cd $SCRATCH
+cp ~/examples/mpi/pi_mc.c ./
+mpicc -o a.out $SCRATCH/pi_mc.c -lm
 
-for i in {2..12..2}
-do
-  echo "With ${i} processes"
-  time mpirun -np $i ./a.out
-done
+echo "With 1 processes"
+time mpirun -np 1 ./a.out 100000
+  
+echo "With 4 processes"
+time mpirun -np 4 ./a.out 100000
+  
+echo "With 9 processes"
+time mpirun -np 9 ./a.out 100000
+  
+echo "With 16 processes"
+time mpirun -np 16 ./a.out 100000
+  
+echo "With 25 processes"
+time mpirun -np 25 ./a.out 100000
+  
+echo "With 36 processes"
+time mpirun -np 36 ./a.out 100000
+  
+echo "With 48 processes"
+time mpirun -np 48 ./a.out 100000
