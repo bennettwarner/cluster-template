248c2d7e86dedac37a25234ad10c0b932103006e
codyfremont <41700866+codyfremont@users.noreply.github.com>
2018-12-10 22:49:13 -0500

    Update Slurm_Job_Test.sh
    modified to /scratch

--- source/Slurm_Job_Test.sh
+++ source/Slurm_Job_Test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -t 1:00:00
-#SBATCH -N 2
+#SBATCH -N 3
 #SBATCH --ntasks-per-node 4
 
 #echo command to stdout
@@ -12,10 +12,10 @@ echo $SLURM_SUBMIT_DIR
 module load mpi/gcc_openmpi
 
 #Assuming hello.c is in $scratch
-cd $scratch
+cd /scratch
 mpicc hello.c
 
-for i in {2..8..2}
+for i in {2..12..2}
 do
   echo "With ${i} processes"
   time mpirun -np $i ./a.out
