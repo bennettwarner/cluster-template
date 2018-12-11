53a79adcf331699da82f2c87a142b2a338517a77
codyfremont <41700866+codyfremont@users.noreply.github.com>
2018-12-10 22:46:11 -0500

    Update Slurm_Job.sh
    edited down to 36, because my DA thought 48 was 7^2
    Also changed $SCRATCH to /scratch

--- source/Slurm_Job.sh
+++ source/Slurm_Job.sh
@@ -12,9 +12,9 @@ echo $SLURM_SUBMIT_DIR
 module load mpi/gcc_openmpi
 
 #may need to be edited for proper dir
-cd $SCRATCH
+cd /scratch
 cp ~/examples/mpi/pi_mc.c ./
-mpicc -o a.out $SCRATCH/pi_mc.c -lm
+mpicc -o a.out /scratch/pi_mc.c -lm
 
 echo "With 1 processes"
 time mpirun -np 1 ./a.out 100000
@@ -33,6 +33,3 @@ time mpirun -np 25 ./a.out 100000
   
 echo "With 36 processes"
 time mpirun -np 36 ./a.out 100000
-  
-echo "With 48 processes"
-time mpirun -np 48 ./a.out 100000
