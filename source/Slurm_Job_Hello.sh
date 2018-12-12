8c46491307498f98e7179f88ad26fef379a4e2e5
codyfremont <41700866+codyfremont@users.noreply.github.com>
2018-12-11 20:59:04 -0500

    Update Slurm_Job_Hello.sh
    scaled to 48

--- source/Slurm_Job_Hello.sh
+++ source/Slurm_Job_Hello.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -t 1:00:00
-#SBATCH -N 3
+#SBATCH -N 12
 #SBATCH --ntasks-per-node 4
 
 #echo command to stdout
@@ -9,14 +9,14 @@ set -x
 # where do I start?
 echo $SLURM_SUBMIT_DIR
 
-#module load mpi/gcc_openmpi
+module load mpi/gcc_openmpi
 
 #Assuming hello.c is in $scratch
 cd /scratch
 mpicc hello.c
 
-for i in {2..12..2}
+for i in {2..48..2}
 do
   echo "With ${i} processes"
-  time mpirun -np $i ./a.out
+  time mpirun --allow-run-as-root -np $i ./a.out
 done
